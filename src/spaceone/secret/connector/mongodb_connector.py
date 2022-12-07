93489c9dbf45aaf152463762806ad3e8339c7069
jiyungSong <bluese05@gmail.com>
2022-12-07 17:09:44 +0900

    feat: add MongoDB Connector
    (cherry picked from commit d16d173b41ee13d7f269ac0b61d065e9168bdc4e)

---
+++ src/spaceone/secret/connector/mongodb_connector.py
@@ -0,0 +1,32 @@
+import logging
+from pymongo import MongoClient
+from spaceone.core.connector import BaseConnector
+
+
+__all__ = ['MongoDBConnector']
+_LOGGER = logging.getLogger(__name__)
+
+
+class MongoDBConnector(BaseConnector):
+
+    def __init__(self, transaction, config):
+        super().__init__(transaction, config)
+        client = MongoClient(**config)
+        db = client.secret_data
+        self.secret_data = db.secret_data
+
+    def create_secret(self, secret_id, data):
+        _document = {'secret_id': secret_id, 'data': data}
+        return self.secret_data.insert_one(_document)
+
+    def delete_secret(self, secret_id):
+        self.secret_data.delete_one({'secret_id': secret_id})
+
+    def update_secret(self, secret_id, data):
+        _query = {'secret_id': secret_id}
+        self.secret_data.update_one(_query, {'$set': {'data': data}})
+
+    def get_secret(self, secret_id):
+        secret_data_vo = self.secret_data.find_one({'secret_id': secret_id})
+        return secret_data_vo
+
